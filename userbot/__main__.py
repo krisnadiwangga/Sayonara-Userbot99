65a58919485f6272edccb6aee56f05b8eadab2be
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-11-20 18:13:30 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔘VEGETA-USERBOT🔘 🔥 V7.1 [AKTIF NGENTOT!!!!]")
+    f"🔘SAYONARA-USERBOT🔘 🔥 V7.1 [TELAH AKTIF !!!!]")
 
 
 if len(argv) not in (1, 3, 4):
