21814a95f30a3879575cfd63d136a5e971cf4e10
GLEDEK-USERBOT <88700407+Kikuk23@users.noreply.github.com>
2021-12-12 07:20:39 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔘SAYONARA-USERBOT🔘 🔥 V7.1 [TELAH AKTIF !!!!]")
+    f"👹 GLEDEK-USERBOT 👹 🔥 V7.1 [TELAH AKTIF !!!!]")
 
 
 if len(argv) not in (1, 3, 4):
