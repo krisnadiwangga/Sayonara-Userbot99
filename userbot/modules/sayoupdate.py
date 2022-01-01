726a32b64853f4dfc40d27d571932679dc70fa58
Krisna Diwangga <89360980+krisnadiwangga@users.noreply.github.com>
2022-01-01 19:49:12 +0700

    Update sayoupdate.py

--- userbot/modules/sayoupdate.py
+++ userbot/modules/sayoupdate.py
@@ -65,7 +65,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                 break
         if heroku_app is None:
             await event.edit(
-                f'{txt}\n`Kredensial Heroku tidak valid untuk deploy RAM-UBOT dyno.`'
+                f'{txt}\n`Kredensial Heroku tidak valid untuk deploy SAYONARA-UBOT dyno.`'
             )
             return repo.__del__()
         await event.edit(f'`{REPO_NAME}:'
