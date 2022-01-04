8e429f615177dee9de7555f7a7daa210bd0f1e09
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-01-05 05:53:08 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -27,7 +27,8 @@ GCAST_BLACKLIST = [
     -1001459701099,  # CatUserbotSupport
     -1001109837870,  # TelegramBotIndonesia
     -1001752592753,  # Skyzusupport
-    -1001273141346, # Remix Support Vegeta
+    -1001273141346,  # Remix Support Vegeta
+    -1001350427774,  # NaraxMusic
 ]
 
 
