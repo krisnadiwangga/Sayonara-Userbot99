c2f61ec90bd577569be439ffb03769c7b7ba5c49
Krisna Diwangga <89360980+krisnadiwangga@users.noreply.github.com>
2022-01-06 11:07:41 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,7 +25,7 @@ async def help(rambot):
         if args in CMD_HELP:
             await rambot.edit(str(CMD_HELP[args]))
         else:
-            await rambot.edit("**`NGETIK YANG BENER NGENTOT!`**")
+            await rambot.edit("**`TIDAK ADA PERINTAH!`**")
             await asyncio.sleep(50)
             await rambot.delete()
     else:
@@ -36,7 +36,7 @@ async def help(rambot):
         await rambot.edit(f"**{REPO_NAME}**\n\n"
                          f"**{EMOJI_HELP} 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**{EMOJI_HELP}  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
                          f"**{EMOJI_HELP} 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ══════════╣❃ ♕ ❃╠══════════\n\n"
-                         f"{EMOJI_HELP} {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
+                         f"{EMOJI_HELP} {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\nNGETIK YANG BENER YA !!\n\n")
         await rambot.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
         await asyncio.sleep(50)
         await rambot.delete()
