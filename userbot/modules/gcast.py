091fbb65442d146f890c3602f8864412ab5ed28c
Cute Inspire <Randywahidin321@gmail.com>
2022-04-07 22:36:16 +0700

    jamet

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,40 +1,33 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-#
-# Ported by Koala @manusiarakitann
-# Recode by @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
-# t.me/SharingUserbot & t.me/Lunatic0de
-# by fix rendy
+import asyncio
 
-from userbot import CMD_HELP, DEVS
 from userbot.events import register
+from userbot import CMD_HELP, DEVS
+
 
 GCAST_BLACKLIST = [
     -1001473548283,  # SharingUserbot
-    -1001433238829,  # TedeSupport
+    -1001752592753,  # TedeSupport
     -1001476936696,  # AnosSupport
     -1001327032795,  # UltroidSupport
     -1001294181499,  # UserBotIndo
     -1001419516987,  # VeezSupportGroup
-    -1001209432070,  # GeezSupportGroup
+    -1001459812644,  # GeezSupportGroup
     -1001296934585,  # X-PROJECT BOT
     -1001481357570,  # UsergeOnTopic
     -1001459701099,  # CatUserbotSupport
     -1001109837870,  # TelegramBotIndonesia
     -1001752592753,  # Skyzusupport
-    -1001273141346,  # Remix Support Vegeta
-    -1001350427774,  # NaraxMusic
+    -1001273141346,  # Erza suppport
+    -1001554560763,  # vegeta support
+    -1001795125065,  # bagas support
+    -1001267233272,  # pocong
+    -1001386557465,  # Kitarosupport
     -1001578091827,  # Prime Support
     -1001380293847,  # NastySupport
 ]
 
-
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
+          pattern=r"^\.cgcast(?: |$)(.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
     if xx:
@@ -44,71 +37,62 @@ async def gcast(event):
     else:
         await event.edit("**Berikan Sebuah Pesan atau Reply**")
         return
-    kk = await event.edit("`Mengirim Pesan Secara Global...`")
+    kk = await event.edit("`Sedang Mengirim Pesan Secara Global... 📢`")
     er = 0
     done = 0
     async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
-            try:
-                if chat not in GCAST_BLACKLIST:
+            if chat not in GCAST_BLACKLIST:
+                try:
+                    await event.client.send_message(chat, msg)
+                    await asyncio.sleep(0.1)
+                    done += 1
+                except FloodWaitError as jamet:
+                    await asyncio.sleep(int(jamet.seconds))
                     await event.client.send_message(chat, msg)
                     done += 1
-                elif chat not in GCAST_BLACKLIST:
-                    pass
-            except BaseException:
-                er += 1
+                except BaseException:
+                    er += 1
     await kk.edit(
         f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**"
     )
 
-
 @register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
 async def gucast(event):
     xx = event.pattern_match.group(1)
-    if xx:
-        msg = xx
-    elif event.is_reply:
-        msg = await event.get_reply_message()
-    else:
-        await event.edit("**Berikan Sebuah Pesan atau Reply**")
-        return
+    if not xx:
+        return await event.edit("`Berikan Sebuah Pesan atau Reply`")
+    tt = event.text
+    msg = tt[7:]
     kk = await event.edit("`Mengirim Pesan Global....`")
     er = 0
     done = 0
     async for x in event.client.iter_dialogs():
         if x.is_user and not x.entity.bot:
             chat = x.id
-            try:
-                if chat not in DEVS:
-                    await event.client.send.message(chat, msg)
-                done += 1
-            except FloodWaitError as e:
-                await asyncio.sleep(e.x)
-                done += 1
-                await event.client.send_message(chat, msg)
-            except BaseException:
-                er += 1
-    await kk.edit(
-        f"**Berhasil Mengirim Pesan Ke** `{done}` **chats, Gagal Mengirim Pesan Ke** `{er}` **chats**"
-    )
+            if chat not in DEVS:
+                try:
+                    await event.client.send_message(chat, msg)
+                    await asyncio.sleep(0.1)
+                    done += 1
+                except FloodWaitError as jamet:
+                    await asyncio.sleep(int(jamet.seconds))
+                    await event.client.send_message(chat, msg)
+                    done += 1
+                except BaseException:
+                    er += 1
+    await kk.edit(f"Berhasil Mengirim Pesan Ke `{done}` obrolan, kesalahan dalam `{er}` obrolan(s)")
 
 
 CMD_HELP.update(
     {
-        "gcast": "**Plugin : **`gcast`\
-        \n\n  •  **Syntax :** `.gcast` <text/reply media>\
-        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Grup yang kamu masuk. (Bisa Mengirim Media/Sticker)\
-    "
-    }
-)
-
+        "gcast": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gcast`\
+         \n↳ : Mengirim Pesan Group Secara Global."})
 
 CMD_HELP.update(
     {
-        "gucast": "**Plugin : **`gucast`\
-        \n\n  •  **Syntax :** `.gucast` <text/reply media>\
-        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)\
-    "
+         "gucast": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gucast`\
+         \n↳ : Mengirim Pesan Pribadi Secara Global."
     }
 )
